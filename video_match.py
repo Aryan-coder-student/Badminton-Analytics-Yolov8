72e2b2a048f8bec8f31b637534c2df0c1b6f8b74
Aryan-coder-student <aryanpahari037@gmail.com>
2025-04-08 19:09:13 +0530

    apply embedding based team classification

--- video_match.py
+++ video_match.py
@@ -1,11 +1,10 @@
 import os 
 import cv2
 import torch
+import numpy as np 
 from more_itertools import chunked
-# Removed unused import
 from sklearn.cluster import KMeans
 import supervision as sv
-# Removed unused import
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
 from transformers import AutoProcessor, AutoModel
@@ -18,12 +17,11 @@ yolo_model = YOLO(model_path)
 
 #---------------------------------------------------------------------------------X-----------------------------------------------------------------
 
-image = cv2.imread(os.path.join("data/badminton.v1i.yolov8/train/images/videoplayback_mp4-0206_jpg.rf.9e4e7aae52f597ef4891305ddcfe91b6.jpg"))
+image = cv2.imread(os.path.join("data/badminton.v1i.yolov8/train/images/videoplayback_mp4-0882_jpg.rf.347258175c6d73e74f9e6e0a72c53ab8.jpg"))
 results = yolo_model(image)[0]
 detections = sv.Detections.from_ultralytics(results)
 REFEREE_CLASS_ID = 1
 filtered_detections = detections[detections.class_id != REFEREE_CLASS_ID]
-
 box_annotation = sv.BoxAnnotator()
 annotated_image = box_annotation.annotate(scene=image.copy(), detections=filtered_detections)
 FOLDER_PATH_ANNOTATED = "Annotated_Image"
@@ -47,7 +45,6 @@ batch_img = chunked(crops, BATCH_SIZE)
 with torch.no_grad():
     for img in batch_img:
         inputs = processor(images=img, return_tensors="pt")
-        print(inputs)
         embeddings = model.get_image_features(**inputs)
         embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=-1)
         embeddings_list.append(embeddings)
@@ -57,18 +54,49 @@ if len(embeddings_list) >= 2:
     kmeans = KMeans(n_clusters=2, random_state=42)
     clusters = kmeans.fit_predict(embeddings_list)
     
-    for i, (crop, cluster_id) in enumerate(zip(crops, clusters)):
-        plt.subplot(1, len(crops), i+1)
-        plt.imshow(crop)
-        plt.title(f"Cluster {cluster_id}")
-        plt.axis('off')
-    plt.show()
+    # for i, (crop, cluster_id) in enumerate(zip(crops, clusters)):
+    #     plt.subplot(1, len(crops), i+1)
+    #     plt.imshow(crop)
+    #     plt.title(f"Cluster {cluster_id}")
+    #     plt.axis('off')
 else:
     print("Need at least 2 players to cluster teams.")
 #---------------------------------------------------------------------------------X------------------------------------------------------------------------
 
+player_location_cluster = dict()
+team_color = {
+    0: (225, 0, 0),  # Red
+    1: (0, 225, 0),  # Green
+}
+print("Called..............")
+for i, (xyxy , cluster_id) in enumerate(zip(filtered_detections.xyxy, clusters)):
+    player_location_cluster[i] = {
+        "xyxy": xyxy,
+        "cluster_id": cluster_id
+    }
 
+box_annotator_player = sv.BoxAnnotator()
+copy_of_image = image.copy()
 
+for plyr_id, track in player_location_cluster.items():
+    ply_detect = sv.Detections(
+        xyxy=np.array([track["xyxy"]]),
+        class_id=np.array([track["cluster_id"]]),
+    )
+    box_annotator = sv.BoxAnnotator(color=sv.Color(*team_color[track["cluster_id"]]))
+    annotated_image = box_annotator.annotate(
+        scene=copy_of_image, detections=ply_detect
+    )
+    label_annotator = sv.LabelAnnotator()
+    annotated_image = label_annotator.annotate(
+        scene=annotated_image,
+        detections=ply_detect,
+        labels=[f"Player {plyr_id} - Team {track['cluster_id']}"],
+    )
+
+output_path = os.path.join(FOLDER_PATH_ANNOTATED, "final_annotated_image.jpg")
+cv2.imwrite(output_path, annotated_image)
+print(f"Annotated image saved at {output_path}")
 
 
 
